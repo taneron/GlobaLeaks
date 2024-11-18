b9078e2e8435e75ccc9e4b02d4fda48a45ffd359
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2024-11-18 19:41:41 +0100

    Separate tests actions to be able to get separated backend and client tests results

---
+++ .github/workflows/scripts/run_tests_backend.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+backend_test_failed=0
+
+setupBackend() {
+  cd $GITHUB_WORKSPACE/backend  # to install backend dependencies
+  pip3 install -r requirements/requirements-$(lsb_release -cs).txt
+}
+
+setupClient() {
+  cd $GITHUB_WORKSPACE/client  # to install frontend dependencies
+  npm install -d
+  ./node_modules/grunt/bin/grunt build_and_instrument
+}
+
+echo "Running setup"
+sudo apt-get update
+sudo apt-get install -y tor
+npm install -g grunt grunt-cli
+pip install coverage
+
+setupBackend
+setupClient
+
+# Running backend tests
+echo "Running backend tests"
+cd $GITHUB_WORKSPACE/backend && coverage run setup.py test
+if [ $? -ne 0 ]; then
+  backend_test_failed=1
+fi
+
+cd $GITHUB_WORKSPACE/backend && coverage xml
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -l Python -r $GITHUB_WORKSPACE/backend/coverage.xml
+
+if [ $backend_test_failed -eq 1 ]; then
+  echo "Backend unit tests: FAILED"
+  exit 1
+else
+  echo "Backend unit tests: PASSED"
+  exit 0
+fi
---
+++ .github/workflows/scripts/run_tests_client.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+client_test_failed=0
+
+setupBackend() {
+  cd $GITHUB_WORKSPACE/backend  # to install backend dependencies
+  pip3 install -r requirements/requirements-$(lsb_release -cs).txt
+}
+
+setupClient() {
+  cd $GITHUB_WORKSPACE/client  # to install frontend dependencies
+  npm install -d
+  ./node_modules/grunt/bin/grunt build_and_instrument
+}
+
+echo "Running setup"
+sudo apt-get update
+sudo apt-get install -y tor
+npm install -g grunt grunt-cli
+setupBackend
+setupClient
+
+$GITHUB_WORKSPACE/backend/bin/globaleaks -z
+sleep 5
+
+# Running client tests locally
+echo "Running client tests locally collecting code coverage"
+cd $GITHUB_WORKSPACE/client && npm test
+if [ $? -ne 0 ]; then
+  client_test_failed=1
+fi
+
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report -l TypeScript -r $GITHUB_WORKSPACE/client/cypress/coverage/lcov.info
+
+if [ $client_test_failed -eq 1 ]; then
+  echo "Client tests: FAILED"
+  exit 1
+else
+  echo "Client tests: PASSED"
+  exit 0
+fi
