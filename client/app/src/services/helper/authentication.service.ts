55632d2c2f861452a525ada162a29498ec6849ca
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2024-11-24 15:46:24 +0100

    Fix bug in relation to loader not getting removed

--- client/app/src/services/helper/authentication.service.ts
+++ client/app/src/services/helper/authentication.service.ts
@@ -112,6 +112,8 @@ export class AuthenticationService {
     requestObservable.subscribe(
       {
         next: (response: Session) => {
+          this.appDataService.updateShowLoadingPanel(false);
+
           if (response.redirect) {
             response.redirect = this.sanitizer.sanitize(SecurityContext.URL, response.redirect) || '';
 	    if (response.redirect) {
@@ -163,7 +165,6 @@ export class AuthenticationService {
                   this.router.navigate([redirect]);
 		}
               } else {
-                this.appDataService.updateShowLoadingPanel(true);
                 this.router.navigate([this.session.homepage], {
                   queryParams: this.activatedRoute.snapshot.queryParams,
                   queryParamsHandling: "merge"
@@ -177,8 +178,8 @@ export class AuthenticationService {
           }
         },
         error: (error: HttpErrorResponse) => {
-          this.loginInProgress = false;
           this.appDataService.updateShowLoadingPanel(false);
+          this.loginInProgress = false;
           if (error.error && error.error["error_code"]) {
             if (error.error["error_code"] === 4) {
               this.requireAuthCode = true;
