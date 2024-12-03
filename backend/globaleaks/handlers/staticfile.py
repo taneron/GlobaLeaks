b1c74bcb77de56011bc25190a8795705ba71558b
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2024-12-03 19:58:12 +0100

    Restrict usage of CSS inline scripts (#4345)

--- backend/globaleaks/handlers/staticfile.py
+++ backend/globaleaks/handlers/staticfile.py
@@ -39,6 +39,6 @@ class StaticFileHandler(BaseHandler):
                                    b"img-src 'self';"
                                    b"media-src 'self';"
                                    b"script-src 'self' 'sha256-l4srTx31TC+tE2K4jVVCnC9XfHivkiSs/v+DPWccDDM=';"
-                                   b"style-src 'self' 'unsafe-inline';")
+                                   b"style-src 'self';")
 
         return self.write_file(filename, abspath)
