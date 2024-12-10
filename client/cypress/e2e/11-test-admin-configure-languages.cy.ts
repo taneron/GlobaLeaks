201ab3b39b1fb062e07f303bea5160df430a1fed
عبدالمنان <msmannan00@protonmail.com>
2024-12-11 00:37:15 +0500

    Fix header interaction after language change and edge case with pre-redirect language update

--- client/cypress/e2e/11-test-admin-configure-languages.cy.ts
+++ client/cypress/e2e/11-test-admin-configure-languages.cy.ts
@@ -58,10 +58,11 @@ describe("admin configure languages", () => {
   it("should reset internationalization texts", () => {
     cy.login_admin();
 
+    cy.waitForUrl("/#/admin/home");
     cy.visit("/#/admin/settings");
     cy.waitForUrl("/#/admin/settings");
+    cy.get('#ngb-nav-6').should('be.visible')
     cy.get('#LanguagePickerBox').should('be.visible').find('ng-select').last().click().get('ng-dropdown-panel').contains('Italian').click();
-    cy.get('#LanguagePickerBox').should('be.visible')
     cy.get('[name="node.dataModel.header_title_homepage"]').should('be.visible').clear();
     cy.get('[name="node.dataModel.presentation"]').should('be.visible').clear();
     cy.get('button.btn.btn-primary').eq(0).get("#save_settings").click();
