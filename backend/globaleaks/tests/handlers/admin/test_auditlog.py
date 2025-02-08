88cf2c7f13f187444bdd204b519a7470e789f546
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2025-02-08 19:21:57 +0100

    [ci] Implement backend unit tests for audit log handler

--- backend/globaleaks/tests/handlers/admin/test_auditlog.py
+++ backend/globaleaks/tests/handlers/admin/test_auditlog.py
@@ -2,7 +2,41 @@
 from twisted.internet.defer import inlineCallbacks
 
 from globaleaks.handlers.admin import auditlog
+from globaleaks.rest import errors
 from globaleaks.tests import helpers
+
+class TestAuditLog(helpers.TestHandlerWithPopulatedDB):
+    _handler = auditlog.AuditLog
+
+    @inlineCallbacks
+    def test_get(self):
+        yield self.perform_full_submission_actions()
+
+        handler = self.request({}, role='admin')
+        response = yield handler.get()
+
+        self.assertTrue(isinstance(response, list))
+        self.assertEqual(len(response), 2)
+
+
+class TestAccessLog(helpers.TestHandlerWithPopulatedDB):
+    _handler = auditlog.AccessLog
+
+    def test_get(self):
+        handler = self.request({}, role='admin')
+
+        # During tests the file does not exists but this is enought to test
+        return self.assertRaises(errors.ResourceNotFound, handler.get)
+
+
+class TestDebugLog(helpers.TestHandlerWithPopulatedDB):
+    _handler = auditlog.DebugLog
+
+    def test_get(self):
+        handler = self.request({}, role='admin')
+
+        # During tests the file does not exists but this is enought to test
+        return self.assertRaises(errors.ResourceNotFound, handler.get)
 
 
 class TestTipsCollection(helpers.TestHandlerWithPopulatedDB):
