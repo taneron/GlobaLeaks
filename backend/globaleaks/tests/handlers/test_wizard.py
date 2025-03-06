f685b2c62ef885b6b175ae40c103825d58469f0e
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2025-03-06 15:36:02 +0100

    Improve unit testing of wizard handler

--- backend/globaleaks/tests/handlers/test_wizard.py
+++ backend/globaleaks/tests/handlers/test_wizard.py
@@ -1,3 +1,5 @@
+import copy
+
 from twisted.internet.defer import inlineCallbacks
 
 from globaleaks import models
@@ -10,10 +12,40 @@ class TestWizard(helpers.TestHandler):
     _handler = wizard.Wizard
 
     @inlineCallbacks
-    def test_post(self):
+    def test_post_config1(self):
         yield self.test_model_count(models.User, 0)
 
         handler = self.request(self.dummyWizard)
+        yield handler.post()
+
+        yield self.test_model_count(models.User, 2)
+
+        # should fail if the wizard has been already completed
+        yield self.assertFailure(handler.post(), errors.ForbiddenOperation)
+
+    @inlineCallbacks
+    def test_post_config2(self):
+        dummyWizardConfig = copy.deepcopy(self.dummyWizard)
+        dummyWizardConfig['skip_recipient_account_creation'] = True
+
+        yield self.test_model_count(models.User, 0)
+
+        handler = self.request(dummyWizardConfig)
+        yield handler.post()
+
+        yield self.test_model_count(models.User, 1)
+
+        # should fail if the wizard has been already completed
+        yield self.assertFailure(handler.post(), errors.ForbiddenOperation)
+
+    @inlineCallbacks
+    def test_post_config2(self):
+        dummyWizardConfig = copy.deepcopy(self.dummyWizard)
+        dummyWizardConfig['admin_escrow'] = False
+
+        yield self.test_model_count(models.User, 0)
+
+        handler = self.request(dummyWizardConfig)
         yield handler.post()
 
         yield self.test_model_count(models.User, 2)
