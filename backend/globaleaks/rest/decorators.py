b6f14486d67585ba887aa6d52c0db0fcb469dd83
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2025-03-10 15:39:03 +0100

    Optimize REST api decorators

--- backend/globaleaks/rest/decorators.py
+++ backend/globaleaks/rest/decorators.py
@@ -1,5 +1,4 @@
 import json
-
 from datetime import timedelta
 from twisted.internet import defer
 from twisted.internet.threads import deferToThread
@@ -13,16 +12,24 @@ from globaleaks.utils.tempdict import TempDict
 from globaleaks.utils.utility import datetime_now, deferred_sleep
 
 
+USERS_ROLES = {'any', 'admin', 'analyst', 'custodian', 'receiver'}
+BYPASS_PATHS = {b"/api/auth/token", b"/api/auth/type", b"/api/report"}
+
+def has_session_or_token(self):
+    return self.token or self.session
+
+
 def decorator_rate_limit(f):
     # Decorator that enforces rate limiting on authenticated whistleblowers' sessions
     def wrapper(self, *args, **kwargs):
         if self.session and self.session.role == 'whistleblower':
             if self.request.path == b'/api/whistleblower/submission':
-                if 1 in State.tenants:
+                root_tenant = State.tenants.get(1)
+                if root_tenant:
                     State.RateLimitingTable.check(self.request.path + b'#' + str(self.request.tid).encode(),
-                                                  State.tenants[1].cache.threshold_reports_per_hour)
+                                                  root_tenant.cache.threshold_reports_per_hour)
                     State.RateLimitingTable.check(self.request.path + b'#' + self.request.client_ip.encode(),
-                                                  State.tenants[1].cache.threshold_reports_per_hour_per_ip)
+                                                  root_tenant.cache.threshold_reports_per_hour_per_ip)
 
             now = datetime_now()
             if now > self.session.ratelimit_time + timedelta(seconds=1):
@@ -32,19 +39,14 @@ def decorator_rate_limit(f):
             self.session.ratelimit_count += 1
 
             if self.session.ratelimit_count > 5:
-                d = deferred_sleep(self.session.ratelimit_count // 5)
-
-                def callback(_):
-                    return f(self, *args, **kwargs)
-
-                d.addCallback(callback)
-
+                wait_time = self.session.ratelimit_count // 5
+                d = deferred_sleep(wait_time)
+                d.addCallback(lambda _: f(self, *args, **kwargs))
                 return d
             else:
                 return f(self, *args, **kwargs)
 
         return f(self, *args, **kwargs)
-
 
     return wrapper
 
@@ -52,7 +54,7 @@ def decorator_rate_limit(f):
 def decorator_require_session_or_token(f):
     # Decorator that ensures a token or a session is included in the request
     def wrapper(self, *args, **kwargs):
-        if not self.request.path in [b"/api/auth/token", b"/api/auth/type", b"/api/report"] and not self.token and not self.session:
+        if self.request.path not in BYPASS_PATHS and not has_session_or_token(self):
             raise errors.InternalServerError("Invalid request: No token and no session")
 
         return f(self, *args, **kwargs)
@@ -63,13 +65,14 @@ def decorator_require_session_or_token(f):
 def decorator_authentication(f, roles):
     # Decorator that performs role checks on the user session
     def wrapper(self, *args, **kwargs):
-        if (('any' in roles) or
-            ((self.session and self.session.tid == self.request.tid) and
-             (('user' in roles and
-               self.session.role in ['admin', 'analyst', 'custodian', 'receiver']) or
-              (self.session.role in roles)))):
-
+        user_roles = set(roles)  # Convert roles to set
+        if 'any' in user_roles:
             return f(self, *args, **kwargs)
+        if self.session and self.session.tid == self.request.tid:
+            if 'user' in user_roles and self.session.role in USERS_ROLES:
+                return f(self, *args, **kwargs)
+            if self.session.role in user_roles:
+                return f(self, *args, **kwargs)
 
         raise errors.NotAuthenticated
 
@@ -82,22 +85,9 @@ def decorator_cache_get(f):
         c = Cache.get(self.request.tid, self.request.path, self.request.language)
         if c is None:
             d = defer.maybeDeferred(f, self, *args, **kwargs)
-
-            def callback(data):
-                if isinstance(data, (dict, list)):
-                    self.request.setHeader(b'content-type', b'application/json')
-                    data = json.dumps(data, cls=JSONEncoder)
-
-                c = self.request.responseHeaders.getRawHeaders(b'Content-type', [b'application/json'])[0]
-                return Cache.set(self.request.tid, self.request.path, self.request.language, c, data)[1]
-
-            d.addCallback(callback)
-
+            d.addCallback(lambda data: Cache.set(self.request.tid, self.request.path, self.request.language, b'application/json', json.dumps(data, cls=JSONEncoder))[1])
             return d
-
-        else:
-            self.request.setHeader(b'Content-type', c[0])
-
+        self.request.setHeader(b'Content-type', c[0])
         return c[1]
 
     return wrapper
