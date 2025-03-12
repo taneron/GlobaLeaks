3e256b14bc8eecb3ada4264a00df312d61292432
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2025-03-12 23:59:17 +0100

    [ci] Extend tests for handlers/recipient/test_recipient.py

--- backend/globaleaks/tests/handlers/recipient/test_recipient.py
+++ backend/globaleaks/tests/handlers/recipient/test_recipient.py
@@ -2,6 +2,7 @@ from twisted.internet.defer import inlineCallbacks
 
 from globaleaks import models
 from globaleaks.handlers import recipient
+from globaleaks.rest import errors
 from globaleaks.tests import helpers
 
 
@@ -29,6 +30,22 @@ class TestOperations(helpers.TestHandlerWithPopulatedDB):
     def setUp(self):
         yield helpers.TestHandlerWithPopulatedDB.setUp(self)
         yield self.perform_full_submission_actions()
+
+    @inlineCallbacks
+    def test_invalid_operation(self):
+        rtips = yield recipient.get_receivertips(1, self.dummyReceiver_1['id'], helpers.USER_PRV_KEY, 'en')
+        rtips_ids = [rtip['id'] for rtip in rtips]
+
+        data_request = {
+            'operation': 'invalid',
+            'args': {
+              'receiver': self.dummyReceiver_2['id'],
+              'rtips': rtips_ids
+            }
+        }
+
+        handler = self.request(data_request, user_id=self.dummyReceiver_1['id'], role='receiver')
+        yield self.assertFailure(handler.put(), errors.ForbiddenOperation)
 
     @inlineCallbacks
     def test_put_revoke_and_grant(self):
