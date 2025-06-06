9c67325192e7c220b4de86cd4ed5a3065a4e2186
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2025-06-06 21:34:59 +0200

    Remove rewrite rule become unnecessary due to usage of CSP policy based on nonces

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -96,21 +96,6 @@ module.exports = function(grunt) {
       },
 
       pass2: {
-        src: "./tmp/js/main.js",
-        dest: "./tmp/js/",
-        options: {
-          replacements: [
-            {
-              pattern: /"ngb-dp-navigation-chevron"/ig,
-              replacement: function () {
-                return "\"fa-solid fa-chevron-right\"";
-              }
-            },
-          ]
-        }
-      },
-
-      pass3: {
         files: {
           "tmp/index.html": "tmp/index.html"
         },
