981fa75f40a6737236c3389fa7e6ec3a54a83087
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2025-09-17 23:24:13 +0200

    [ci] Remove configurations of cypress that are currently unused
    In fact we are currently not performing any component testing but just end2end testing

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,13 +1,6 @@
 import {defineConfig} from "cypress";
 
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "angular",
-      bundler: "webpack",
-    },
-    specPattern: "**/*.cy.ts",
-  },
   env: {
     "coverage": true,
     "language": "en",
