1332712a31bb98983f678fcc6542a16449edc0de
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2025-09-17 23:24:50 +0200

    Update cypress configuration deprecating plugins/index.ts and using up-to-date recomendations

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,4 +1,9 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 import {defineConfig} from "cypress";
+import registerCodeCoverageTasks from "@cypress/code-coverage/task";
+
 
 export default defineConfig({
   env: {
@@ -27,7 +32,50 @@ export default defineConfig({
   },
   e2e: {
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.ts").default(on, config);
+      // All your plugin logic goes here
+      registerCodeCoverageTasks(on, config);
+
+      on("before:browser:launch", (browser, launchOptions) => {
+        if (browser.family === "chromium") {
+          launchOptions.args.push("--window-size=1920,1080");
+          launchOptions.args.push("--force-device-scale-factor=1");
+          launchOptions.args.push("--force-color-profile=srgb");
+          launchOptions.args.push("--disable-low-res-tiling");
+          launchOptions.args.push("--disable-smooth-scrolling");
+        }
+        return launchOptions;
+      });
+
+      on("after:screenshot", (details) => {
+        if (details.path.includes("failed")) return;
+
+        const language = config.env.language;
+        const destPath = path.resolve(
+          __dirname,
+          "../documentation/images",
+          details.path.replace(".png", "").split("/").slice(-2).join("/") +
+            "." +
+            language +
+            ".png"
+        );
+        const destDir = path.dirname(destPath);
+        if (!fs.existsSync(destDir)) fs.mkdirSync(destDir, { recursive: true });
+        fs.copyFileSync(details.path, destPath);
+        return { path: destPath };
+      });
+
+      on("task", {
+        log(message) {
+          console.log(message);
+          return null;
+        },
+        table(message) {
+          console.table(message);
+          return null;
+        },
+      });
+
+      return config;
     },
     baseUrl: "https://127.0.0.1:8443",
     viewportWidth: 1920,
