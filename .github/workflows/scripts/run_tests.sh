bd41e7b1719e202af2f0603ba74c0625320356df
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2025-10-04 16:45:45 +0200

    [ci] Reorganize tests to fix coverage collection

--- .github/workflows/scripts/run_tests.sh
+++ .github/workflows/scripts/run_tests.sh
@@ -16,19 +16,19 @@ npm install -d
 ./node_modules/grunt/bin/grunt build_for_testing_and_instrument
 
 cd $GITHUB_WORKSPACE/backend && coverage run -m twisted.trial globaleaks.tests
-cd $GITHUB_WORKSPACE/backend && coverage lcov -o $GITHUB_WORKSPACE/backend/lcov.info
-
 cd $GITHUB_WORKSPACE/backend && coverage run --append ./bin/globaleaks -z -n &
 
-sleep 5
+sleep 3
 
 # Running client tests locally
 echo "Running client tests locally collecting code coverage"
 cd $GITHUB_WORKSPACE/client && npm test
 
-sleep 5
+sleep 3
 
-sudo killall coverage --wait
+killall coverage --wait
+
+sleep 3
 
 cd $GITHUB_WORKSPACE/backend && coverage lcov -o $GITHUB_WORKSPACE/backend/lcov.info
 sed -i 's|SF:globaleaks/|SF:backend/globaleaks/|g' $GITHUB_WORKSPACE/backend/lcov.info
