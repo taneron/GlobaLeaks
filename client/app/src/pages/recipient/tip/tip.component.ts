d0a08f6e2022a02017282d3adddb0fcc8c7f6789
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2025-11-25 22:47:22 +0100

    Revise texts and icons used within the application

--- client/app/src/pages/recipient/tip/tip.component.ts
+++ client/app/src/pages/recipient/tip/tip.component.ts
@@ -1,4 +1,5 @@
 import {ChangeDetectorRef, Component, OnInit, TemplateRef, ViewChild, inject} from "@angular/core";
+import {FormsModule} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {AppConfigService} from "@app/services/root/app-config.service";
 import {TipService} from "@app/shared/services/tip-service";
@@ -46,29 +47,30 @@ import {TranslatorPipe} from "@app/shared/pipes/translate";
     templateUrl: "./tip.component.html",
     standalone: true,
     imports: [
-    NgClass,
-    TipInfoComponent,
-    TipReceiverListComponent,
-    TipQuestionnaireAnswersComponent,
-    WhistleBlowerIdentityReceiverComponent,
-    TipFilesReceiverComponent,
-    NgbNav,
-    NgbNavItem,
-    NgbNavItemRole,
-    NgbNavLinkButton,
-    NgbNavLinkBase,
-    NgbNavContent,
-    NgTemplateOutlet,
-    NgbNavOutlet,
-    NgbTooltipModule,
-    NgbDropdown,
-    NgbDropdownToggle,
-    NgbDropdownMenu,
-    TipUploadWbFileComponent_1,
-    TipCommentsComponent_1,
-    TranslateModule,
-    TranslatorPipe
-],
+      FormsModule,
+      NgClass,
+      TipInfoComponent,
+      TipReceiverListComponent,
+      TipQuestionnaireAnswersComponent,
+      WhistleBlowerIdentityReceiverComponent,
+      TipFilesReceiverComponent,
+      NgbNav,
+      NgbNavItem,
+      NgbNavItemRole,
+      NgbNavLinkButton,
+      NgbNavLinkBase,
+      NgbNavContent,
+      NgTemplateOutlet,
+      NgbNavOutlet,
+      NgbTooltipModule,
+      NgbDropdown,
+      NgbDropdownToggle,
+      NgbDropdownMenu,
+      TipUploadWbFileComponent_1,
+      TipCommentsComponent_1,
+      TranslateModule,
+      TranslatorPipe
+    ],
 })
 export class TipComponent implements OnInit {
   private translateService = inject(TranslateService);
@@ -104,11 +106,11 @@ export class TipComponent implements OnInit {
   submission: any;
 
   ngOnInit() {
-    this.loadTipDate();
+    this.loadTipData();
     this.cdr.detectChanges();
   }
 
-  loadTipDate() {
+  loadTipData() {
     this.tip_id = this.activatedRoute.snapshot.paramMap.get("tip_id");
     this.redactOperationTitle = this.translateService.instant('Mask') + ' / ' + this.translateService.instant('Redact');
     const requestObservable: Observable<any> = this.httpService.receiverTip(this.tip_id);
@@ -157,6 +159,11 @@ export class TipComponent implements OnInit {
           component: this.tab3
         },
       ];
+    });
+  }
+
+  updateLabel(label: string) {
+    this.httpService.tipOperation("set", {"key": "label", "value": label}, this.RTipService.tip.id).subscribe(() => {
     });
   }
 
@@ -406,7 +413,7 @@ export class TipComponent implements OnInit {
   }
 
   listenToFields() {
-    this.loadTipDate();
+    this.loadTipData();
   }
 
   protected readonly JSON = JSON;
--- client/app/src/shared/partials/tip-info/tip-info.component.ts
+++ client/app/src/shared/partials/tip-info/tip-info.component.ts
@@ -4,7 +4,6 @@ import {WbtipService} from "@app/services/helper/wbtip.service";
 import {AppDataService} from "@app/app-data.service";
 import {UtilsService} from "@app/shared/services/utils.service";
 import {ReceiverTipService} from "@app/services/helper/receiver-tip.service";
-import {HttpService} from "@app/shared/services/http.service";
 import {DatePipe} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {TranslateModule} from "@ngx-translate/core";
@@ -22,8 +21,6 @@ export class TipInfoComponent {
   protected authenticationService = inject(AuthenticationService);
   protected appDataService = inject(AppDataService);
   protected utilsService = inject(UtilsService);
-  private rTipService = inject(ReceiverTipService);
-  private httpService = inject(HttpService);
 
   @Input() tipService: ReceiverTipService | WbtipService;
   @Input() loading: boolean;
@@ -33,9 +30,4 @@ export class TipInfoComponent {
     const current_date = new Date();
     return current_date > report_date;
   };
-
-  updateLabel(label: string) {
-    this.httpService.tipOperation("set", {"key": "label", "value": label}, this.rTipService.tip.id).subscribe(() => {
-    });
-  }
 }
